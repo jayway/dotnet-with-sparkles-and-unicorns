c24306a145397d10cedfc79ac520cee6d84c1015
Tomas Lycken <tlycken@users.noreply.github.com>
2017-10-12 14:49:26 +0200

    State at start of demo 6

---
+++ demos/demo6/ConsoleApp.NetFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp.NetFramework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
