0adea0d2fb43dddac91fa891efa24aa2f2a9e2be
Tomas Lycken <tlycken@users.noreply.github.com>
2017-10-12 15:38:40 +0200

    Converted library

---
+++ demos/demo6/ClassLibrary.NetCoreSDK/Foo.cs
@@ -0,0 +1,10 @@
+﻿using ClassLibrary.NetFramework.Bar;
+using Newtonsoft.Json;
+
+namespace ClassLibrary.NetFramework
+{
+    public class Foo
+    {
+        public string DoStuff() => JsonConvert.SerializeObject(new Baz());
+    }
+}
